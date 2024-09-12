3ebb4126cf5b486228e05c2c853949f4f56894d8
MohammedIzhar77 <izharmohammed21@gmail.com>
2024-09-12 21:28:04 +0530

    Dockerized backend

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,33 @@
+import { PrismaClient } from "@prisma/client";
 import express from "express";
 const app = express();
+const prisma = new PrismaClient();
+app.use(express.json());
+//cors
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+app.get('/test', (req, res) => {
+    try {
+        res.status(200).json({ msg: "Api working" })
+    } catch (error) {
+        res.status(500).json({ msg: "Some error in api" })
+    }
 
-app.get('/', (req, res) => {
-    res.json({
-        msg: "successfull!!"
-    })
 })
 
-app.listen(4000, () => {
-    console.log('server is up!!!');
 
+app.get('/users', async (req, res) => {
+    const users = await prisma.user.findMany()
+    res.json( users )
+})
+
+
+
+
+app.listen(3000, () => {
+    console.log('server is up!!!');
 })
